305ef4e6c914bcb63a7337f24206bf2781c74ef3
wjq20061111 <wjq20061111@163.com>
2017-05-24 17:14:03 +0800

    init

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <iostream>
+
+//pcl 1.7
+#include <pcl/io/pcd_io.h>
+#include <pcl/io/ply_io.h>
+#include <pcl/point_cloud.h>
+#include <pcl/console/parse.h>
+#include <pcl/common/transforms.h>
+#include <pcl/visualization/pcl_visualizer.h>
+
+#endif // !MAIN_H
