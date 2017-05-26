7b16f04fbba60c1f2a7f9ae7b798e65373c917b2
wjq20061111 <wjq20061111@163.com>
2017-05-26 10:27:19 +0800

    add viewer

--- main.cpp
+++ main.cpp
@@ -1,129 +1,181 @@
 #include "main.h"
 
-// This function displays the help
-void
-showHelp(char * program_name)
+typedef pcl::PointCloud<pcl::PointXYZ> CloudType;
+
+void pclviewer(const CloudType::Ptr  &cloud);
+
+int main (int argc, char** argv)
 {
-  std::cout << std::endl;
-  std::cout << "Usage: " << program_name << " cloud_filename.[pcd|ply]" << std::endl;
-  std::cout << "-h:  Show this help." << std::endl;
+	libfreenect2::Freenect2 freenect2;
+	libfreenect2::Freenect2Device *dev = 0;
+	libfreenect2::PacketPipeline *pipeline = 0;
+	std::string serial = "";
+
+	if(freenect2.enumerateDevices() == 0)
+	{
+		std::cout << "no device connected!" << std::endl;
+		return -1;
+	}
+	if (serial == "")
+	{
+		serial = freenect2.getDefaultDeviceSerialNumber();
+	}
+	pipeline = new libfreenect2::CpuPacketPipeline();//other choices omitted
+	
+	if(pipeline)
+	{
+		dev = freenect2.openDevice(serial, pipeline);
+	}
+	else 
+	{
+		dev = freenect2.openDevice(serial);
+	}
+	if(dev == 0)
+	{
+		std::cout << "failure opening device!" << std::endl;
+		return -1;
+	}
+	else
+	{
+		std::cout << "device standby" << std::endl;
+	}
+
+	bool enable_rgb = true;
+	bool enable_depth = true;
+	int types = 0;
+	if (enable_rgb)
+		types |= libfreenect2::Frame::Color;
+	if (enable_depth)
+		types |= libfreenect2::Frame::Ir | libfreenect2::Frame::Depth;
+	libfreenect2::SyncMultiFrameListener listener(types);
+	libfreenect2::FrameMap frames;
+	dev->setColorFrameListener(&listener);
+	dev->setIrAndDepthFrameListener(&listener);
+	std::cout << "listener ready" << std::endl;
+
+	if (enable_rgb && enable_depth)
+	{
+		if (!dev->start())//true if ok, false if error.
+			return -1;
+	}
+	else
+	{
+		if (!dev->startStreams(enable_rgb, enable_depth))
+			return -1;
+	}
+	std::cout << "device serial: " << dev->getSerialNumber() << std::endl;
+	std::cout << "device firmware: " << dev->getFirmwareVersion() << std::endl;
+	
+	libfreenect2::Registration* registration = new libfreenect2::Registration(dev->getIrCameraParams(), dev->getColorCameraParams());
+	libfreenect2::Frame undistorted(512, 424, 4), registered(512, 424, 4),depth2rgb(1920, 1080 + 2, 4);
+	
+	Mat rgbmat,depthmat,irmat;
+	Mat depthmatUndistorted,rgbd,rgbd2;
+	std::cout << "while start" << std::endl;	
+	bool protonect_shutdown = false; 
+	size_t framemax = 1;
+	size_t framecount = 0;
+
+
+	CloudType::Ptr cloud (new CloudType);
+
+	while(!protonect_shutdown && (framemax == (size_t)-1 || framecount < framemax))
+	{
+		if (!listener.waitForNewFrame(frames, 10*1000)) // 10 sconds
+		{
+			std::cout << "timeout!" << std::endl;
+			return -1;
+		}
+		libfreenect2::Frame *rgb = frames[libfreenect2::Frame::Color];
+		libfreenect2::Frame *ir = frames[libfreenect2::Frame::Ir];
+		libfreenect2::Frame *depth = frames[libfreenect2::Frame::Depth];
+		
+		cv::Mat(rgb->height, rgb->width, CV_8UC4, rgb->data).copyTo(rgbmat);
+		cv::Mat(depth->height, depth->width, CV_32FC1, depth->data).copyTo(depthmat);
+		cv::Mat(ir->height, ir->width, CV_32FC1, ir->data).copyTo(irmat);
+		registration->apply(rgb, depth, &undistorted, &registered, true, &depth2rgb);
+		cv::Mat(undistorted.height, undistorted.width, CV_32FC1, undistorted.data).copyTo(depthmatUndistorted);
+		cv::Mat(registered.height, registered.width, CV_8UC4, registered.data).copyTo(rgbd);
+		cv::Mat(depth2rgb.height, depth2rgb.width, CV_32FC1, depth2rgb.data).copyTo(rgbd2);
+
+		float cx=0,cy=0,fx=1,fy=1;
+		float xu,yu;
+		CloudType::PointType p;
+		//p.x = 1; p.y = 2; p.z = 3;
+		// for(int xi=0;xi<512;xi++)
+		// {
+		// 	for(int yi=0;yi<424;yi++)
+		// 	{
+		// 		xu=(xi+0.5-cx)/fx;
+		// 		yu=(yi+0.5-cy)/fy;
+
+		// 		p.x = xu*undistorted.data[512*yi+xi];
+		// 		p.y= yu*undistorted.data[512*yi+xi];
+		// 		p.z = undistorted.data[512*yi+xi];
+		// //RGB = registered[512*yi+xi];
+		// 	cloud->push_back(p);
+		// 	}
+		// }
+		for(int r=0;r<512;r++)
+		{
+			for(int c=0;c<424;c++)
+			{
+			float point3d[3];
+			registration->getPointXYZ(&undistorted,r,c,point3d[0],point3d[1],point3d[2]);
+			p.x = point3d[0];
+			p.y = point3d[1];
+			p.z = point3d[2];
+			cloud->push_back(p);
+			}
+		}
+	// Load file | Works with PCD and PLY files
+
+	// Visualization
+	// 	pcl::visualization::PCLVisualizer viewer ("My example");
+
+	//  // Define R,G,B colors for the point cloud
+	// 	pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> cloud_color_handler (cloud, 255, 255, 255);
+	// // We add the point cloud to the viewer and pass the color handler
+	// 	viewer.addPointCloud (cloud, cloud_color_handler, "original_cloud");
+
+
+	// 	viewer.addCoordinateSystem (1.0, "cloud", 0);
+	// 	viewer.setBackgroundColor(0.05, 0.05, 0.05, 0); // Setting background to a dark grey
+	// 	viewer.setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 2, "original_cloud");
+
+	// 	while (!viewer.wasStopped ()) { // Display the visualiser until 'q' key is pressed
+	// 		viewer.spinOnce ();
+	// 	}
+	pclviewer(cloud);
+	
+	framecount++;
+	std::cout << framecount<<std::endl;
+
+	int key = cv::waitKey(0);
+	listener.release(frames);
+	}
+
+	dev->stop();
+	dev->close();
+	delete registration;
+	return 0;
 }
 
-// This is the main function
-int
-main (int argc, char** argv)
+void pclviewer(const CloudType::Ptr  &cloud)
 {
+		pcl::visualization::PCLVisualizer viewer ("My example");
 
-  // Show help
-  if (pcl::console::find_switch (argc, argv, "-h") || pcl::console::find_switch (argc, argv, "--help")) {
-    showHelp (argv[0]);
-    return 0;
-  }
+	 // Define R,G,B colors for the point cloud
+		pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> cloud_color_handler (cloud, 255, 255, 255);
+	// We add the point cloud to the viewer and pass the color handler
+		viewer.addPointCloud (cloud, cloud_color_handler, "original_cloud");
 
-  // Fetch point cloud filename in arguments | Works with PCD and PLY files
-  std::vector<int> filenames;
-  bool file_is_pcd = false;
 
-  filenames = pcl::console::parse_file_extension_argument (argc, argv, ".ply");
+		viewer.addCoordinateSystem (1.0, "cloud", 0);
+		viewer.setBackgroundColor(0.05, 0.05, 0.05, 0); // Setting background to a dark grey
+		viewer.setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 2, "original_cloud");
 
-  if (filenames.size () != 1)  {
-    filenames = pcl::console::parse_file_extension_argument (argc, argv, ".pcd");
-
-    if (filenames.size () != 1) {
-      showHelp (argv[0]);
-      return -1;
-    } else {
-      file_is_pcd = true;
-    }
-  }
-
-  // Load file | Works with PCD and PLY files
-  pcl::PointCloud<pcl::PointXYZ>::Ptr source_cloud (new pcl::PointCloud<pcl::PointXYZ> ());
-
-  if (file_is_pcd) {
-    if (pcl::io::loadPCDFile (argv[filenames[0]], *source_cloud) < 0)  {
-      std::cout << "Error loading point cloud " << argv[filenames[0]] << std::endl << std::endl;
-      showHelp (argv[0]);
-      return -1;
-    }
-  } else {
-    if (pcl::io::loadPLYFile (argv[filenames[0]], *source_cloud) < 0)  {
-      std::cout << "Error loading point cloud " << argv[filenames[0]] << std::endl << std::endl;
-      showHelp (argv[0]);
-      return -1;
-    }
-  }
-
-  /* Reminder: how transformation matrices work :
-
-           |-------> This column is the translation
-    | 1 0 0 x |  \
-    | 0 1 0 y |   }-> The identity 3x3 matrix (no rotation) on the left
-    | 0 0 1 z |  /
-    | 0 0 0 1 |    -> We do not use this line (and it has to stay 0,0,0,1)
-
-    METHOD #1: Using a Matrix4f
-    This is the "manual" method, perfect to understand but error prone !
-  */
-  Eigen::Matrix4f transform_1 = Eigen::Matrix4f::Identity();
-
-  // Define a rotation matrix (see https://en.wikipedia.org/wiki/Rotation_matrix)
-  float theta = M_PI/4; // The angle of rotation in radians
-  transform_1 (0,0) = cos (theta);
-  transform_1 (0,1) = -sin(theta);
-  transform_1 (1,0) = sin (theta);
-  transform_1 (1,1) = cos (theta);
-  //    (row, column)
-
-  // Define a translation of 2.5 meters on the x axis.
-  transform_1 (0,3) = 2.5;
-
-  // Print the transformation
-  printf ("Method #1: using a Matrix4f\n");
-  std::cout << transform_1 << std::endl;
-
-  /*  METHOD #2: Using a Affine3f
-    This method is easier and less error prone
-  */
-  Eigen::Affine3f transform_2 = Eigen::Affine3f::Identity();
-
-  // Define a translation of 2.5 meters on the x axis.
-  transform_2.translation() << 2.5, 0.0, 0.0;
-
-  // The same rotation matrix as before; theta radians arround Z axis
-  transform_2.rotate (Eigen::AngleAxisf (theta, Eigen::Vector3f::UnitZ()));
-
-  // Print the transformation
-  printf ("\nMethod #2: using an Affine3f\n");
-  std::cout << transform_2.matrix() << std::endl;
-
-  // Executing the transformation
-  pcl::PointCloud<pcl::PointXYZ>::Ptr transformed_cloud (new pcl::PointCloud<pcl::PointXYZ> ());
-  // You can either apply transform_1 or transform_2; they are the same
-  pcl::transformPointCloud (*source_cloud, *transformed_cloud, transform_2);
-
-  // Visualization
-  printf(  "\nPoint cloud colors :  white  = original point cloud\n"
-      "                        red  = transformed point cloud\n");
-  pcl::visualization::PCLVisualizer viewer ("Matrix transformation example");
-
-   // Define R,G,B colors for the point cloud
-  pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> source_cloud_color_handler (source_cloud, 255, 255, 255);
-  // We add the point cloud to the viewer and pass the color handler
-  viewer.addPointCloud (source_cloud, source_cloud_color_handler, "original_cloud");
-
-  pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> transformed_cloud_color_handler (transformed_cloud, 230, 20, 20); // Red
-  viewer.addPointCloud (transformed_cloud, transformed_cloud_color_handler, "transformed_cloud");
-
-  viewer.addCoordinateSystem (1.0, "cloud", 0);
-  viewer.setBackgroundColor(0.05, 0.05, 0.05, 0); // Setting background to a dark grey
-  viewer.setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 2, "original_cloud");
-  viewer.setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 2, "transformed_cloud");
-  //viewer.setPosition(800, 400); // Setting visualiser window position
-
-  while (!viewer.wasStopped ()) { // Display the visualiser until 'q' key is pressed
-    viewer.spinOnce ();
-  }
-
-  return 0;
+		while (!viewer.wasStopped ()) { // Display the visualiser until 'q' key is pressed
+			viewer.spinOnce ();
+		}
 }
